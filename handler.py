48a2fb5fdc4b240f5bda9c3e36d1ccd3129f5ba0
Adithya Saish <adithya.work99@gmail.com>
2021-05-05 00:19:59 +0530

    added a query string params

--- handler.py
+++ handler.py
@@ -2,9 +2,14 @@ import json
 
 
 def hello(event, context):
+    input=event['queryStringParameters']['type']
+    if input==1:
+        output=1
+    else:
+        output=2
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input":event
+        "input":output
     }
     response = {
         "statusCode": 200,
